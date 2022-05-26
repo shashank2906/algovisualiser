013a8a591b8631f688aa99df048200eb094684c9
shashank rai <shashank.rai00331@gmail.com>
2022-05-26 23:09:33 +0530

    final-struct

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,4 @@
 
-
-/*
-Variable naming convention: <object>_<action>_<objectname>; Example -> Button_click_b1;
-*/
-
-//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
 var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
 var inp_gen=document.getElementById("a_generate");
 var inp_aspeed=document.getElementById("a_speed");
@@ -67,6 +61,45 @@ function disable_buttons()
     }
 }
 
+
+
+
+
+
+
+function enable_buttons()
+{
+    window.setTimeout(function(){
+        for(var i=0;i<butts_algos.length;i++)
+        {
+            butts_algos[i].classList=[];
+            butts_algos[i].classList.add("butt_unselected");
+
+            butts_algos[i].disabled=false;
+            inp_as.disabled=false;
+            inp_gen.disabled=false;
+            inp_aspeed.disabled=false;
+        }
+    },c_delay+=delay_time);
+}
+
+
+
+
+
+
+function div_update(cont,height,color)
+{
+    window.setTimeout(function(){
+        cont.style=" margin:0% " + margin_size + "%; width:" + (100/array_size-(2*margin_size)) + "%; height:" + height + "%; background-color:" + color + ";";
+    },c_delay+=delay_time);
+}
+
+
+
+
+
+
 function runalgo()
 {
     disable_buttons();
@@ -88,4 +121,47 @@ function runalgo()
                         break;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var speed=100;
+
+inp_aspeed.addEventListener("input",vis_speed);
+
+function vis_speed()
+{
+    var array_speed=inp_aspeed.value;
+    switch(parseInt(array_speed))
+    {
+        case 1: speed=1;
+                break;
+        case 2: speed=10;
+                break;
+        case 3: speed=100;
+                break;
+        case 4: speed=1000;
+                break;
+        case 5: speed=10000;
+                break;
+    }
+    
+    delay_time=10000/(Math.floor(array_size/10)*speed);        //Decrease numerator to increase speed.
+}
+
+var delay_time=10000/(Math.floor(array_size/10)*speed);        //Decrease numerator to increase speed.
+var c_delay=0;//This is updated ov every div change so that visualization is visible.
+
+
 
