4158baa342d064d0a61f4d64e94b1ef0ce2ee043
shashank rai <shashank.rai00331@gmail.com>
2022-05-29 15:01:23 +0530

    ui updated

---
+++ scripts/search/binary.js
@@ -0,0 +1,25 @@
+
+function Binary()
+{
+    c_delay=0;
+    let m = div_sizes[6];
+
+    for(var i=0;i<array_size-1;i++)
+    {
+        
+            div_update(divs[i],div_sizes[i],"yellow");//Color update
+
+            if(div_sizes[i] == m)
+            {
+                div_update(divs[i],div_sizes[i], "red");//Color update
+               break;
+            }
+            
+        div_update(divs[i],div_sizes[i], "red");//Color update
+    }
+    div_update(divs[0],div_sizes[0], "red");//Color update
+    // these are colors after color update 
+
+    enable_buttons();
+}
+
